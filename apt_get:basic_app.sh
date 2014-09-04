87abadc783b827fe5d5d23ca96369819fa9635e4
Yukun Huang <huangyukun2012@gmail.com>
2014-09-04 10:26:48 +0800

    Update apt_get:basic_app.sh

--- apt_get:basic_app.sh
+++ apt_get:basic_app.sh
@@ -1,4 +1,6 @@
 apt-get install -y vim git 
+echo "\n\nvim git installed\n\n"
 apt-get install -y gcc g++
+echo "\n\n gcc g++\n\n"
 apt-get install -y libnet-ifconfig-wrapper-perl inetutils-ping
-
+echo "\n\nnet tool\n\n"
